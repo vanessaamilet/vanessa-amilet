6843b6c82c4bfd849109cbd0d3373548aaf4d9c8
Vanessa Santos <vanessa.santos@hcss.com>
2018-01-31 12:06:11 -0600

    Added smooth scrolling js

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,7 +1,32 @@
+/* ## Preloader
+---------------------------------------------------------------------------------------------------- */
+
 $(window).load(function() {
-    $("#status").delay(2e3).fadeOut("slow"),
-    $("#preloader").delay(1600).fadeOut("slow")
+	$("#status").delay(2e3).fadeOut("slow"),
+	$("#preloader").delay(1600).fadeOut("slow")
 }),
 $(window).scroll(function() {
-    $(this).scrollTop() > 50 ? $(".before-color").addClass("after-color") : $(".before-color").removeClass("after-color")
+	$(this).scrollTop() > 50 ? $(".before-color").addClass("after-color") : $(".before-color").removeClass("after-color")
+});
+
+/* ## Smooth Scrolling
+---------------------------------------------------------------------------------------------------- */
+
+jQuery(document).ready(function($) {
+
+	$('a[href^="#"]').click(function(event) {
+		event.preventDefault(); 
+
+		var defaultAnchorOffset = 0;
+		
+		var $anchor = $('#' + this.hash.substring(1));
+				
+		var anchorOffset = $anchor.data('anchor-offset');
+		if (!anchorOffset) // for when anchor doesn't have the offset attribute like Section 4
+			anchorOffset = defaultAnchorOffset; 
+
+		$('html,body').animate({ 
+			scrollTop: $anchor.offset().top - anchorOffset
+		}, 700);        
+	});
 });
