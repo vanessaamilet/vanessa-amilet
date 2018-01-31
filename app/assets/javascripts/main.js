1f906a8eb5daf127b984bf6e46bbd9c199144f6a
Vanessa Santos <vanessa.santos@hcss.com>
2018-01-31 15:04:28 -0600

    Made minor updates to css & js files

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -3,7 +3,7 @@
 
 $(window).load(function() {
 	$("#status").delay(2e3).fadeOut("slow"),
-	$("#preloader").delay(1600).fadeOut("slow")
+	$("#preloader").delay(1400).fadeOut("slow")
 }),
 $(window).scroll(function() {
 	$(this).scrollTop() > 50 ? $(".before-color").addClass("after-color") : $(".before-color").removeClass("after-color")
