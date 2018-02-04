a87a7597665d0e4b6f4db49da6cdff741a9b4159
Vanessa Santos <vanessa.santos@hcss.com>
2018-02-04 13:42:27 -0600

    Updated some portfolio pgs and added showcases

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,3 +1,12 @@
+/* ## Navigation
+---------------------------------------------------------------------------------------------------- */
+
+$('#toggle').click(function() {
+	$(this).toggleClass('active');
+
+	$('#overlay').toggleClass('open');
+});
+
 /* ## Preloader
 ---------------------------------------------------------------------------------------------------- */
 
@@ -12,21 +21,50 @@ $(window).scroll(function() {
 /* ## Smooth Scrolling
 ---------------------------------------------------------------------------------------------------- */
 
-jQuery(document).ready(function($) {
+$(function() {
+    $('a[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
+});
 
-	$('a[href^="#"]').click(function(event) {
-		event.preventDefault(); 
 
-		var defaultAnchorOffset = 0;
-		
-		var $anchor = $('#' + this.hash.substring(1));
-				
-		var anchorOffset = $anchor.data('anchor-offset');
-		if (!anchorOffset) // for when anchor doesn't have the offset attribute like Section 4
-			anchorOffset = defaultAnchorOffset; 
+/* ## Scroll to Top
+---------------------------------------------------------------------------------------------------- */
 
-		$('html,body').animate({ 
-			scrollTop: $anchor.offset().top - anchorOffset
-		}, 700);        
+$(document).ready(function(){
+	// Scroll (in pixels) after which the "To Top" link is shown
+	var offset = 300,
+		//Scroll (in pixels) after which the "back to top" link opacity is reduced
+		offset_opacity = 1200,
+		//Duration of the top scrolling animation (in ms)
+		scroll_top_duration = 700,
+		//Get the "To Top" link
+		$back_to_top = $('.to-top');
+
+	//Visible or not "To Top" link
+	$(window).scroll(function(){
+		( $(this).scrollTop() > offset ) ? $back_to_top.addClass('top-is-visible') : $back_to_top.removeClass('top-is-visible top-fade-out');
+		if( $(this).scrollTop() > offset_opacity ) { 
+			$back_to_top.addClass('top-fade-out');
+		}
 	});
+
+	//Smoothy scroll to top
+	$back_to_top.on('click', function(event){
+		event.preventDefault();
+		$('body,html').animate({
+			scrollTop: 0 ,
+		 	}, scroll_top_duration
+		);
+	});
+
 });
