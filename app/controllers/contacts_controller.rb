ef78daac874fa21b2eed7ca275fd8ca2358c98a7
Vanessa Santos <vanessa.santos@hcss.com>
2018-02-15 10:09:07 -0600

    Updated Snapple pg

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,16 +1,17 @@
 class ContactsController < ApplicationController
   def new
-	@contact = Contact.new
-  end
+      @contact = Contact.new
+    end
 
-  def create
-	@contact = Contact.new(params[:contact])
-	@contact.request = request
-	if @contact.deliver
-	  flash.now[:notice] = 'Thank you for your message. I will contact you soon!'
-	else
-	  flash.now[:error] = 'Cannot send message.'
-	  render :new
-	end
-  end
+    def create
+      @contact = Contact.new(params[:contact])
+      @contact.request = request
+      if @contact.deliver
+      	flash.now[:error] = nil
+        flash.now[:notice] = 'Thank you for your message. We will contact you soon!'
+      else
+        flash.now[:error] = 'Cannot send message.'
+        render :new
+      end
+    end
 end
