3f535204a6c19d3863001c49a7910a111517ec61
Vanessa Santos <vanessa.santos@hcss.com>
2018-02-19 14:32:15 -0600

    Implemented Mailgun w/ Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,17 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Setup for Mailgun
+  # config.action_mailer.default_url_options = { :host => 'vanessa-amilet.herokuapp.com' }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :authentication => :plain,
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => 'vanessa-amilet.herokuapp.com',
+  }
 end
