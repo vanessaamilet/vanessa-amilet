bd3bd1cfe087383bea6eb750bc13167d4fd15642
Vanessa Santos <vanessa.santos@hcss.com>
2018-02-20 10:02:59 -0600

    updated contact controller

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -7,15 +7,18 @@ class ContactsController < ApplicationController
       @contact = Contact.new(contact_params)
       @contact.request = request
       if @contact.deliver
-      	flash.now[:error] = nil
-        flash.now[:notice] = 'Thank you for your message. We will contact you soon!'
+      	# flash.now[:error] = nil
+       	# flash.now[:notice] = 'Thank you for your message. I will contact you soon!'
+       	flash[:success] = "Thank you for your message. I will contact you soon!"
+     	redirect_to contact_path
       else
-        flash.now[:error] = 'There was an error sending your message. Please try again.'
-        render :new
+      	#flash.now[:error] = 'There was an error sending your message. Please try again.'
+      	#render :new
+      	redirect_to contact_path, notice: "There was an error sending your message. Please try again."
       end
     end
-
-    def contact_params
-    	params.require(:contact).permit(:name, :email, :phone_number, :message, :nickname)
-	end
+    private
+	    def contact_params
+	    	params.require(:contact).permit(:name, :email, :phone_number, :message, :nickname)
+		end
 end
--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,8 +1,9 @@
 class ContactMailer < ActionMailer::Base
   default to: 'vanessa.amilet@gmail.com'
-  def contact_email(name, email, body)
+  def contact_email(name, email, phone, body)
     @name = name
     @email = email
+    @phone = phone_number
     @body = body
     mail(from: email, subject: 'Contact Form Message')
   end
