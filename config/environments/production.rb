fb0106985e3f55f6c3a9c58059fcb861ef714362
vanessaamilet <vanessa.amilet@gmail.com>
2020-06-09 20:35:44 -0500

    Minor updates

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -115,13 +115,24 @@ Rails.application.configure do
   # Setup for Mailgun
     # config.action_mailer.default_url_options = { :host => 'vanessa-amilet.herokuapp.com' }
   
-    config.action_mailer.delivery_method = :smtp
-    config.action_mailer.smtp_settings = {
-      :authentication => :plain,
+    # config.action_mailer.delivery_method = :smtp
+    # config.action_mailer.smtp_settings = {
+    #   :authentication => :plain,
+    #   :port           => ENV['MAILGUN_SMTP_PORT'],
+    #   :address        => ENV['MAILGUN_SMTP_SERVER'],
+    #   :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    #   :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    #   :domain         => 'vanessa-amilet.herokuapp.com',
+    #   :authentication => :plain,
+    # }
+
+    ActionMailer::Base.smtp_settings = {
       :port           => ENV['MAILGUN_SMTP_PORT'],
       :address        => ENV['MAILGUN_SMTP_SERVER'],
       :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
       :password       => ENV['MAILGUN_SMTP_PASSWORD'],
       :domain         => 'vanessa-amilet.herokuapp.com',
+      :authentication => :plain,
     }
+    ActionMailer::Base.delivery_method = :smtp
 end
