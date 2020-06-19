c8b8eae814294d747e74689db3fc976f1fec1782
vanessaamilet <vanessa.amilet@gmail.com>
2020-06-18 21:47:03 -0500

    Minor updates to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,10 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  # protect_from_forgery with: :exception
+
+  skip_before_action :verify_authenticity_token
+  protect_from_forgery prepend: true, with: :exception
 
   # URL Redirect
   APP_DOMAIN = 'www.amilet.com'
