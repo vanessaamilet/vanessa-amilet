98d68a60162a2424d4151d9fd59a77c0576115db
Vanessa Amilet Santos <vanessa.amilet@gmail.com>
2026-01-06 21:33:38 +0000

    Portfolio updates

--- app/helpers/portfolio_helper.rb
+++ app/helpers/portfolio_helper.rb
@@ -1,17 +1,75 @@
 module PortfolioHelper
 	def all_projects
 	  [
-		{ title: "Firmatek", path: portfolio_firmatek_path, img: "portfolio/portfolio-firmatek-website.jpg", label: "Web Design + Development (WordPress)" },
-		{ title: "OutWellness ATX", path: portfolio_outwellness_atx_path, img: "portfolio/portfolio-outwellness-atx.jpg", label: "Web Design + Development (WordPress)" },
-		{ title: "Capitalize", path: portfolio_capitalize_path, img: "portfolio/portfolio-capitalize.jpg", label: "Website Development (WordPress)" },
-		{ title: "Nonprofit Website", path: portfolio_foundation_website_path, img: "portfolio/portfolio-nonprofit-website.jpg", label: "Web Design + Development (WordPress)" },
-		{ title: "MFour Mobile Research", path: portfolio_mfour_path, img: "portfolio/portfolio-mfour-1.jpg", label: "Website Development (WordPress)" },
-		{ title: "Community Impact Website", path: portfolio_community_impact_website_path, img: "portfolio/portfolio-community-impact-website.jpg", label: "Website Development (WordPress)" },
-		{ title: "Youth Career Resource Website", path: portfolio_youthcareer_path, img: "portfolio/portfolio-youthcareer-website.jpg", label: "Web Design + Development (WordPress)" },
-		{ title: "Compost & Soil Products Website", path: portfolio_greenerworld_path, img: "portfolio/portfolio-compost-soil-website.jpg", label: "Web Design + Development (WordPress)" },
-		{ title: "Electrical Industry Partnership Website", path: portfolio_electricalalliance_path, img: "portfolio/portfolio-electrical-industry-website.jpg", label: "Web Design + Development (WordPress)" },
-		{ title: "Marketing Agency Website", path: portfolio_agency_path, img: "portfolio/portfolio-agency-website.jpg", label: "Website Development (WordPress)" },
-		{ title: "Events by Picnics in the City", path: portfolio_picnics_path, img: "portfolio/portfolio-picnicsinthecity.jpg", label: "Web Design + Development (Shopify)" },
+		{ 
+			title: "Firmatek", 
+			path: portfolio_firmatek_path, 
+			img: "portfolio/portfolio-firmatek-website.jpg", 
+			label: "Web Design + Development (WordPress)",
+			services: %w[web-design development],
+			project_type: %w[website],
+			industry: %w[technology construction] 
+		},
+		{ 
+			title: "OutWellness ATX", 
+			path: portfolio_outwellness_atx_path, 
+			img: "portfolio/portfolio-outwellness-atx.jpg", 
+			label: "Web Design + Development (WordPress)" 
+		},
+		{ 
+			title: "Capitalize", 
+			path: portfolio_capitalize_path, 
+			img: "portfolio/portfolio-capitalize.jpg", 
+			label: "Website Development (WordPress)" 
+		},
+		{ 
+			title: "Nonprofit Website", 
+			path: portfolio_foundation_website_path, 
+			img: "portfolio/portfolio-nonprofit-website.jpg", 
+			label: "Web Design + Development (WordPress)" 
+		},
+		{ 
+			title: "MFour Mobile Research", 
+			path: portfolio_mfour_path, 
+			img: "portfolio/portfolio-mfour-1.jpg", 
+			label: "Website Development (WordPress)" 
+		},
+		{ 
+			title: "Community Impact Website", 
+			path: portfolio_community_impact_website_path, 
+			img: "portfolio/portfolio-community-impact-website.jpg", 
+			label: "Website Development (WordPress)" 
+		},
+		{ 
+			title: "Youth Career Resource Website", 
+			path: portfolio_youthcareer_path, 
+			img: "portfolio/portfolio-youthcareer-website.jpg", 
+			label: "Web Design + Development (WordPress)" 
+		},
+		{ 
+			title: "Compost & Soil Products Website", 
+			path: portfolio_greenerworld_path, 
+			img: "portfolio/portfolio-compost-soil-website.jpg", 
+			label: "Web Design + Development (WordPress)" 
+		},
+		{ 
+			title: "Electrical Industry Partnership Website", 
+			path: portfolio_electricalalliance_path, 
+			img: "portfolio/portfolio-electrical-industry-website.jpg", 
+			label: "Web Design + Development (WordPress)" 
+		},
+		{ 
+			title: "Marketing Agency Website", 
+			path: portfolio_agency_path, 
+			img: "portfolio/portfolio-agency-website.jpg", 
+			label: "Website Development (WordPress)" 
+		},
+		{ 
+			title: "Events by Picnics in the City", 
+			path: portfolio_picnics_path, 
+			img: "portfolio/portfolio-picnicsinthecity.jpg", 
+			label: "Web Design + Development (Shopify)" 
+		},
 		{ title: "Zack Pride", path: portfolio_zackpride_path, img: "portfolio/portfolio-zackpride.jpg", label: "Web Design + Development (WordPress)" },
 		{ title: "Maryland Local Business Collective Website", path: portfolio_community_path, img: "portfolio/portfolio-community-market-website.jpg", label: "Web Design + Development (WordPress)" },
 		{ title: "Genie Creates", path: portfolio_genie_path, img: "portfolio/portfolio-genie.jpg", label: "Web Design + Development (WordPress)" },
