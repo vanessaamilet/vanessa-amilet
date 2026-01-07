b669e522d0430ffe988c28db6b579266986e1b21
Vanessa Amilet Santos <vanessa.amilet@gmail.com>
2026-01-07 22:49:12 +0000

    Portfolio updates

--- app/helpers/portfolio_helper.rb
+++ app/helpers/portfolio_helper.rb
@@ -1,6 +1,7 @@
 module PortfolioHelper
 	def all_projects
 	  [
+		# Row 1
 		{ 
 			title: "Geospatial Data & Insights Platform Website", 
 			path: portfolio_firmatek_path, 
@@ -25,6 +26,7 @@ module PortfolioHelper
 			label: "Website Development (WordPress)",
 			desc: "Redesigned to support evolving branding and improve content flexibility." 
 		},
+		# Row 2
 		{ 
 			title: "Nonprofit Foundation Website", 
 			path: portfolio_foundation_website_path, 
@@ -40,12 +42,12 @@ module PortfolioHelper
 			desc: "Rebuilt to support scalable content, modern branding, and product storytelling." 
 		},
 		{ 
-			title: "Community Impact Website", 
-			path: portfolio_community_impact_website_path, 
-			img: "portfolio/portfolio-community-impact-website.jpg", 
-			label: "Website Development (WordPress)",
-			desc: "Restructured to improve navigation and access to essential resources."  
+			title: "Cloud Storage Management Web App", path: portfolio_jumpix_path, 
+			img: "portfolio/portfolio-jumpix.jpg", 
+			label: "Web Design + Development (Ruby on Rails), UX Design, Media",
+			desc: "Designed a welcoming site aligned with values of inclusivity and care."  
 		},
+		# Row 2
 		{ 
 			title: "Youth Career Resource Website", 
 			path: portfolio_youthcareer_path, 
@@ -67,6 +69,29 @@ module PortfolioHelper
 			label: "Web Design + Development (WordPress)",
 			desc: "Redesigned to reflect brand credibility and improve access to training resources."
 		},
+		# Row 4
+		{ 
+			title: "Community Impact Website", 
+			path: portfolio_community_impact_website_path, 
+			img: "portfolio/portfolio-community-impact-website.jpg", 
+			label: "Website Development (WordPress)",
+			desc: "Restructured to improve navigation and access to essential resources."  
+		},
+		{ 
+			title: "Cosmetics Brand & Ecommerce Launch",
+			path: portfolio_captivecolour_path,
+			img: "portfolio/portfolio-captivecolour.jpg",
+			label: "Branding, Web Design + Development (Shopify)",
+			desc: "Developed brand identity and e-commerce presence for product launch."  
+		},
+		{ 
+			title: "Energy Home Security Marketing Campaign",
+			path: portfolio_reliantsecurity_path,
+			img: "portfolio/portfolio-relaint-security.jpg",
+			label: "Web Design + Development, Motion Graphics",
+			desc: "Designed campaign assets to promote home security offerings across digital channels."
+		},
+		# Row 5
 		{ 
 			title: "Content Marketing Agency Website", 
 			path: portfolio_agency_path, 
@@ -75,11 +100,11 @@ module PortfolioHelper
 			desc: "Refreshed to showcase services and strengthen brand positioning."
 		},
 		{ 
-			title: "Events by Picnics in the City", 
-			path: portfolio_picnics_path, 
-			img: "portfolio/portfolio-picnicsinthecity.jpg", 
-			label: "Web Design + Development (Shopify)",
-			desc: "Updated to highlight expanded services and elevate the customer journey."
+			title: "Maryland Local Business Collective Website", 
+			path: portfolio_community_path, 
+			img: "portfolio/portfolio-community-market-website.jpg", 
+			label: "Web Design + Development (WordPress)",
+			desc: "Created an online presence to showcase local businesses ahead of retail launch."
 		},
 		{
 			title: "Hispanic Market Energy Campaign Landing Pages",
@@ -91,79 +116,13 @@ module PortfolioHelper
 			project_type: %w[campaign landing-page],
 			industry: %w[security sales]
 		},
+		# Row 6
 		{ 
-			title: "Zack Pride - Musician Portfolio Website", 
-			path: portfolio_zackpride_path, 
-			img: "portfolio/portfolio-zackpride.jpg", 
-			label: "Web Design + Development (WordPress)",
-			desc: "Designed a central hub for performances, press, and events."
-		 },
-		{ 
-			title: "Maryland Local Business Collective Website", 
-			path: portfolio_community_path, 
-			img: "portfolio/portfolio-community-market-website.jpg", 
-			label: "Web Design + Development (WordPress)",
-			desc: "Created an online presence to showcase local businesses ahead of retail launch."
-		 },
-		{ 
-			title: "Construction Marketing Agency Website", path: portfolio_genie_path,
-			img: "portfolio/portfolio-genie.jpg",
-			label: "Web Design + Development (WordPress)",
-			desc: "Designed a clean, professional site to showcase services and industry expertise."
-		 },
-		{ 
-			title: "Industrial Energy Services Website", 
-			path: portfolio_ucs_path, 
-			img: "portfolio/portfolio-ucs.jpg", 
-			label: "Web Design + Development (WordPress)",
-			desc: "Redesigned to reflect new branding and clearly present services." 
-		},
-		{
-			title: "Mar y Sol Therapy & Wellness", 
-			path: portfolio_marysol_path, 
-			img: "portfolio/portfolio-marysol.jpg", 
-			label: "Web Design + Development (WordPress)",
-			desc: "Designed a welcoming site aligned with values of inclusivity and care." 
-		},
-		{ 
-			title: "Cloud Storage Management Web App", path: portfolio_jumpix_path, 
-			img: "portfolio/portfolio-jumpix.jpg", 
-			label: "Web Design + Development (Ruby on Rails), UX Design, Media",
-			desc: "Designed a welcoming site aligned with values of inclusivity and care."  
-		},
-		{ 
-			title: "Consulting Firm Landing Page", path: portfolio_leewardteam_path, 
-			img: "portfolio/portfolio-leewardteam.jpg", 
-			label: "Landing Page Design + Development (WordPress)",
-			desc: "Built a clean landing page to reestablish digital presence."  
-		},
-		{ 
-			title: "Betty Boujee", 
-			path: portfolio_bettyboujee_path, 
-			img: "portfolio/portfolio-bettyboujee.jpg", 
+			title: "Events by Picnics in the City", 
+			path: portfolio_picnics_path, 
+			img: "portfolio/portfolio-picnicsinthecity.jpg", 
 			label: "Web Design + Development (Shopify)",
-			desc: "Designed and developed a brand-forward website to guide users from discovery to inquiry."
-		},
-		{ 
-			title: "Expertly Sanitized", 
-			path: portfolio_expertly_path, 
-			img: "portfolio/portfolio-expertlysanitized.jpg", 
-			label: "Web Design + Development (WordPress)",
-			desc: "Modernized the site to better represent services and improve usability."
-		},
-		{ 
-			title: "Opera Artist Website Design", 
-			path: portfolio_jarrodlee_path, 
-			img: "portfolio/portfolio-jarrodlee.jpg", 
-			label: "Web Design + Development (WordPress)",
-			desc: "Created an easy-to-manage site for events, press, and media." 
-		},
-		{ 
-			title: "Cosmetics Brand & Ecommerce Launch",
-			path: portfolio_captivecolour_path,
-			img: "portfolio/portfolio-captivecolour.jpg",
-			label: "Branding, Web Design + Development (Shopify)",
-			desc: "Developed brand identity and e-commerce presence for product launch."  
+			desc: "Updated to highlight expanded services and elevate the customer journey."
 		},
 		{ 
 			title: "I Build America", 
@@ -173,32 +132,83 @@ module PortfolioHelper
 			desc: "Redesigned to improve the customer journey and highlight members and projects."  
 		},
 		{ 
+			title: "Construction Intern Awards",
+			path: portfolio_cia_path,
+			img: "portfolio/portfolio-cia.jpg", 
+			label: "Web Design + Development (WordPress)",
+			desc: "Redesigned and developed a high-traffic campaign site to engage students and sponsors."
+		},
+		# Row 7
+		{ 
+			title: "Betty Boujee", 
+			path: portfolio_bettyboujee_path, 
+			img: "portfolio/portfolio-bettyboujee.jpg", 
+			label: "Web Design + Development (Shopify)",
+			desc: "Designed and developed a brand-forward website to guide users from discovery to inquiry."
+		},
+		{ 
+			title: "Industrial Energy Services Website", 
+			path: portfolio_ucs_path, 
+			img: "portfolio/portfolio-ucs.jpg", 
+			label: "Web Design + Development (WordPress)",
+			desc: "Redesigned to reflect new branding and clearly present services." 
+		},
+		{ 
+			title: "Zack Pride - Musician Portfolio Website", 
+			path: portfolio_zackpride_path, 
+			img: "portfolio/portfolio-zackpride.jpg", 
+			label: "Web Design + Development (WordPress)",
+			desc: "Designed a central hub for performances, press, and events."
+		},
+		# Row 8
+		{ 
+			title: "Construction Marketing Agency Website", path: portfolio_genie_path,
+			img: "portfolio/portfolio-genie.jpg",
+			label: "Web Design + Development (WordPress)",
+			desc: "Designed a clean, professional site to showcase services and industry expertise."
+		 },
+		{
+			title: "Mar y Sol Therapy & Wellness", 
+			path: portfolio_marysol_path, 
+			img: "portfolio/portfolio-marysol.jpg", 
+			label: "Web Design + Development (WordPress)",
+			desc: "Designed a welcoming site aligned with values of inclusivity and care." 
+		},
+		{ 
+			title: "Expertly Sanitized", 
+			path: portfolio_expertly_path, 
+			img: "portfolio/portfolio-expertlysanitized.jpg", 
+			label: "Web Design + Development (WordPress)",
+			desc: "Modernized the site to better represent services and improve usability."
+		},
+		# Row 8
+		{ 
+			title: "Consulting Firm Landing Page", path: portfolio_leewardteam_path, 
+			img: "portfolio/portfolio-leewardteam.jpg", 
+			label: "Landing Page Design + Development (WordPress)",
+			desc: "Built a clean landing page to reestablish digital presence."  
+		},
+		{ 
+			title: "Opera Artist Website Design", 
+			path: portfolio_jarrodlee_path, 
+			img: "portfolio/portfolio-jarrodlee.jpg", 
+			label: "Web Design + Development (WordPress)",
+			desc: "Created an easy-to-manage site for events, press, and media." 
+		},
+		{ 
 			title: "Creative Studio Website Design", 
 			path: portfolio_igco_path, 
 			img: "portfolio/portfolio-igco.jpg", 
 			label: "Web Design + Development (WordPress)",
 			desc: "Designed and developed a website to showcase work, services, and creative expertise."
 		},
-		{ 
-			title: "Energy Home Security Marketing Campaign",
-			path: portfolio_reliantsecurity_path,
-			img: "portfolio/portfolio-relaint-security.jpg",
-			label: "Web Design + Development, Motion Graphics",
-			desc: "Designed campaign assets to promote home security offerings across digital channels."
-		},
+		# Row 8
 		{ 
 			title: "North Bloom - Commercial Construction Website", 
 			path: portfolio_northbloom_path, 
 			img: "portfolio/portfolio-northbloom.jpg", 
 			label: "Web Design + Development (WordPress)",
 			desc: "Developed a custom construction website to showcase services and project experience."
-		},
-		{ 
-			title: "Construction Intern Awards",
-			path: portfolio_cia_path,
-			img: "portfolio/portfolio-cia.jpg", 
-			label: "Web Design + Development (WordPress)",
-			desc: "Redesigned and developed a high-traffic campaign site to engage students and sponsors."
 		},
 	  ]
 	end
